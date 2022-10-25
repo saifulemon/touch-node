0a8c0bb51d3c95e1bd3243cd1103439404e95205
saifulemon <463saiful0@gmail.com>
2022-10-25 15:27:59 +0600

    use query parameter

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello from my second ever node');
 });
 
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
+
 const users = [
   {id:0, name:'Safiul Islam', email:'dev.saifulemon@gmail.com', phone:'01302050981'},
   {id:1, name:'Saikat Noman', email:'SaikatNoman@gmail.com', phone:'01302055444'},
@@ -14,17 +18,26 @@ const users = [
   {id:4, name:'Mahir Sahriar', email:'MahirSahriar@gmail.com', phone:'013056415481'},
   {id:5, name:'Meherul Ahmed', email:'MeherulAhmed', phone:'04872514581'}
 ]
-
 app.get('/users', (req, res) => {
-  res.send(users);
+  const search = req.query.search;
+  // use query parameter
+  if(search) {
+    const searchResult = users.filter(user => user.name.toLowerCase().includes(search));
+    res.send(searchResult);
+  }
+  else {
+    res.send(users);
+  }
 });
 
+// dynamic API
 app.get('/users/:id', (req, res) => {
   const id = req.params.id;
   const user = users[id];
   res.send(user);
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-});
+
+app.get('/fruits', (req, res) => {
+  res.send(['mango', 'banana', 'Apple', 'melon', 'orange']);
+})
