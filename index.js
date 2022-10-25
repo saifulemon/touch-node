b3848f03a7f252a205e7d4eccccf32a47aeac2b7
saifulemon <463saiful0@gmail.com>
2022-10-25 17:53:02 +0600

    sending data from express to react project is successful

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const app = express();
 const port = 2000;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello from my second ever node');
